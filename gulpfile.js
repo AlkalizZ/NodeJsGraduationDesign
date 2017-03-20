37d6b0af0b66baf7086f53c25e3ca5ec9ef3bb0a
AlkalizZ <799478793@qq.com>
2017-03-20 17:30:24 +0800

    3.20 下班打卡

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,22 @@
 var gulp = require('gulp');
 
-gulp.task('default', function() {
-  // 将你的默认的任务代码放在这
+gulp.task('default', () => {
+  // 默认任务
   console.log(123);
+});
+gulp.task('init', () => {
+  // 生成基础文件夹及文件
+
+});
+gulp.task('new', () => {
+  // 新建md文件
+});
+gulp.task('generate', () => {
+  // 生成public文件夹及文件
+});
+gulp.task('g', () => {
+    gulp.run('generate');
+});
+gulp.task('clean', () => {
+  // 清除public文件夹
 });
