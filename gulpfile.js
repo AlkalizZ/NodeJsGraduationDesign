a1f1c48b4bad1af6822c384d55c6419622d4efbc
AlkalizZ <799478793@qq.com>
2017-03-21 17:28:57 +0800

    3.21 下班打卡，新建文件功能需改进

--- gulpfile.js
+++ gulpfile.js
@@ -2,24 +2,24 @@ var gulp = require('gulp');
 var index = require('./bin/index');
 
 gulp.task('default', () => {
-  // 默认任务
-  console.log(123);
+    // 默认任务
+    console.log('this is default task');
 });
 gulp.task('init', () => {
-  // 生成基础文件夹及文件
-
+    // 生成基础文件夹及文件
+    index.generate('./public/js');
 });
 gulp.task('new', () => {
-  // 新建md文件
+    // 新建md文件
 });
 gulp.task('generate', () => {
-  // 生成public文件夹及文件
-});
-gulp.task('g', () => {
-    gulp.run('generate');
+    // 生成public文件夹及文件
+    index.clean('public');
+    index.generate('./public/js');
+    index.generate('./public/img');
+    index.generate('./public/css');
 });
 gulp.task('clean', () => {
-  // 清除public文件夹
-    
+    // 清除public文件夹
     index.clean('public');
 });
