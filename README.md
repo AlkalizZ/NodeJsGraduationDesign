42ed7f190d88176f4956bc1d180450e14bd440db
AlkalizZ <799478793@qq.com>
2017-04-06 17:34:14 +0800

    命令new完成，init待完成

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # NodeJsGraduationDesign
 毕业设计
+
+## 系统初始化
+gulp init
+
+## 新建文档
+gulp new --title [title]
--- gulpfile.js
+++ gulpfile.js
@@ -1,25 +1,43 @@
 var gulp = require('gulp');
 var index = require('./bin/index');
+var fs = require('fs');
+
 
 gulp.task('default', () => {
     // 默认任务
     console.log('this is default task');
 });
+
+//TODO 生成基础文件夹及文件
 gulp.task('init', () => {
-    // 生成基础文件夹及文件
-    index.generate('./public/js');
+    fs.readFile('./assets/_config.yml', 'utf-8', (err, data) => {
+        if(err){
+            console.log(err);
+        }
+        index.generate('./_config.yml', data);
+    });
 });
+
+// 新建md文件
 gulp.task('new', () => {
-    // 新建md文件
+    var argv = require('minimist')(process.argv.slice(2));
+    fs.readFile('./assets/scaffolds/post.md', 'utf-8', (err, data) => {
+        if(err){
+            console.log(err);
+        }
+        index.generate(`./source/_post/${argv.title}.md`, data);
+    });
 });
+
+// 生成public文件夹及文件
 gulp.task('generate', () => {
-    // 生成public文件夹及文件
     index.clean('public');
     index.generate('./public/js');
     index.generate('./public/img');
     index.generate('./public/css');
 });
+
+// 清除public文件夹
 gulp.task('clean', () => {
-    // 清除public文件夹
     index.clean('public');
 });
