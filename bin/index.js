fe60fdbbc4ae6bd3b6e9c3d8dd0b9f6588cacff8
AlkalizZ <799478793@qq.com>
2017-05-09 01:17:39 +0800

    数组去重函数

--- bin/index.js
+++ bin/index.js
@@ -8,9 +8,11 @@ var fileOperations =  require('../lib/fileOperations');
 var clean = require('../lib/clean');
 var marked = require('../lib/parse');
 var getRandom = require('../lib/getRandom');
+var unique = require('../lib/unique');
 
 exports.clean = clean;
 exports.generate = generate;
 exports.marked = marked;
 exports.fsExistsSync = fileOperations.fsExistsSync;
 exports.getRandom = getRandom;
+exports.unique = unique;
